6f84f2eceb57b56081911157830c2e67d34457bd
Lokre <franciscomaureirap@gmail.com>
2024-08-26 15:30:03 -0400

    ke wea

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.hosts << "iic2143-docker.onrender.com" ## Dar persmiso a la url de render iic2143-docker.onrender.com 
+
+
   # Do not eager load code on boot.
   config.eager_load = false
 
